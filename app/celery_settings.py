937c66dcf2f0607a7054c41b110a9fadcf653d0b
pancham <panchamrb@phonepe.com>
2019-01-10 14:28:55 +0530

    WIP async

--- app/celery_settings.py
+++
@@ -1,3 +0,0 @@
-import os
-
-BROKER_URL = os.environ.get('REDIS_URL')
--- app/tasks.py
+++ app/tasks.py
@@ -5,7 +5,6 @@ from app.common.constants_and_variables import AppVariables
 app_variables = AppVariables()
 
 app = Celery()
-# app.config_from_object("celery_settings")
 app.conf.BROKER_URL = app_variables.redis_url
 
 
