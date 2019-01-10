e3df1cdd4de8ac3226f664463d4bb974f6d39a49
pancham <panchamrb@phonepe.com>
2019-01-10 14:13:31 +0530

    WIP async

--- tasks.py
+++ tasks.py
@@ -1,9 +1,11 @@
-import os
-
 from celery import Celery
 
+from app.common.constants_and_variables import AppVariables
+
+app_variables = AppVariables()
+
 app = Celery()
-app.conf.broker_url(os.environ.get('REDIS_URL'))
+app.conf.broker_url(app_variables.redis_url)
 
 
 @app.task
