e7c44c9235d253b086e400b1e2c8870158fbf60f
pancham <panchamrb@phonepe.com>
2019-01-10 14:53:48 +0530

    WIP async

--- app/app.py
+++ app/app.py
@@ -36,6 +36,4 @@ def strava_webhook():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    logger = logging.getLogger(__name__)
     app.run(host=app_variables.app_host, port=int(app_variables.app_port), debug=app_variables.app_debug)
