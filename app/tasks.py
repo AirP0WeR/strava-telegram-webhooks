3a5f1b0a09d4b7225f008c1ea7c9087e76a8ce4a
pancham <panchamrb@phonepe.com>
2019-01-10 14:27:20 +0530

    WIP async

--- app/tasks.py
+++ app/tasks.py
@@ -5,7 +5,9 @@ from app.common.constants_and_variables import AppVariables
 app_variables = AppVariables()
 
 app = Celery()
-app.config_from_object("celery_settings")
+# app.config_from_object("celery_settings")
+app.conf.BROKER_URL = app_variables.redis_url
+
 
 @app.task
 def hello():
