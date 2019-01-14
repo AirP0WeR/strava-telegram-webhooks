746942584ebe526a00f95113955fcfdf4524a1b0
pancham <panchamrb@phonepe.com>
2019-01-14 21:26:07 +0530

    Exception Handling

--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
 #  -*- encoding: utf-8 -*-
 
+import logging
+import traceback
+
 from flask import Flask, request, jsonify
 
 from app.common.constants_and_variables import AppVariables, AppConstants
+from app.common.shadow_mode import ShadowMode
 from app.tasks import update_stats, update_indoor_ride, update_all_stats
 
 app_variables = AppVariables()
 app_constants = AppConstants()
+shadow_mode = ShadowMode()
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -14,30 +19,45 @@ app.config.from_object(__name__)
 
 @app.route("/stats/<athlete_id>", methods=['POST'])
 def stats(athlete_id):
-    if request.method == 'POST':
-        update_stats.delay(athlete_id)
-        return jsonify(''), 200
+    try:
+        if request.method == 'POST':
+            update_stats.delay(athlete_id)
+            return jsonify(''), 200
+    except Exception:
+        message = "Something went wrong. Exception: {exception}".format(exception=traceback.format_exc())
+        logging.error(message)
+        shadow_mode.send_message(message)
 
 
 @app.route("/stats/all", methods=['POST'])
 def stats_for_all():
-    if request.method == 'POST':
-        update_all_stats.delay()
-        return jsonify(''), 200
+    try:
+        if request.method == 'POST':
+            update_all_stats.delay()
+            return jsonify(''), 200
+    except Exception:
+        message = "Something went wrong. Exception: {exception}".format(exception=traceback.format_exc())
+        logging.error(message)
+        shadow_mode.send_message(message)
 
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def strava_webhook():
-    if request.method == 'POST':
-        message = request.json
-        update_stats.delay(message['owner_id'])
-        if message['aspect_type'] == "create" and message['object_type'] == "activity":
-            update_indoor_ride.delay(message['owner_id'], message['object_id'])
-        return jsonify(''), 200
+    try:
+        if request.method == 'POST':
+            message = request.json
+            update_stats.delay(message['owner_id'])
+            if message['aspect_type'] == "create" and message['object_type'] == "activity":
+                update_indoor_ride.delay(message['owner_id'], message['object_id'])
+            return jsonify(''), 200
 
-    elif request.method == 'GET':
-        hub_challenge = request.args.get('hub.challenge')
-        return jsonify({'hub.challenge': hub_challenge}), 200
+        elif request.method == 'GET':
+            hub_challenge = request.args.get('hub.challenge')
+            return jsonify({'hub.challenge': hub_challenge}), 200
+    except Exception:
+        message = "Something went wrong. Exception: {exception}".format(exception=traceback.format_exc())
+        logging.error(message)
+        shadow_mode.send_message(message)
 
 
 if __name__ == '__main__':
