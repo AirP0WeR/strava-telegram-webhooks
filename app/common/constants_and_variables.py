3c73ad2085511ac7c9564ed7dbecc3d2d98da47c
Pancham Bharadwaj <panchamrb@gmail.com>
2019-03-05 18:57:11 +0530

    Act only on active athletes

--- app/common/constants_and_variables.py
+++ app/common/constants_and_variables.py
@@ -4,12 +4,12 @@ import os
 
 
 class AppConstants(object):
-    QUERY_FETCH_TOKEN_NAME_TELEGRAM_NAME = "select access_token, refresh_token, expires_at, name, telegram_username from strava_telegram_bot where athlete_id={athlete_id}"
+    QUERY_FETCH_TOKEN_NAME_TELEGRAM_NAME = "select access_token, refresh_token, expires_at, name, telegram_username from strava_telegram_bot where active=true and athlete_id={athlete_id}"
     QUERY_UPDATE_TOKEN = "UPDATE strava_telegram_bot SET access_token='{access_token}', refresh_token='{refresh_token}', expires_at={expires_at}, updated=now() where athlete_id={athlete_id}"
     QUERY_UPDATE_STRAVA_DATA = "UPDATE strava_telegram_bot SET name='{name}', strava_data='{strava_data}', updated=now() WHERE athlete_id={athlete_id}"
     QUERY_FETCH_UPDATE_INDOOR_RIDE = "select update_indoor_ride, update_indoor_ride_data, chat_id from strava_telegram_bot where athlete_id={athlete_id}"
     QUERY_ACTIVITY_SUMMARY = "select enable_activity_summary, chat_id from strava_telegram_bot where athlete_id={athlete_id}"
-    QUERY_FETCH_ALL_ATHLETE_IDS = "select athlete_id from strava_telegram_bot"
+    QUERY_FETCH_ALL_ATHLETE_IDS = "select athlete_id from strava_telegram_bot where active=true"
     QUERY_DEACTIVATE_ATHLETE = "UPDATE strava_telegram_bot SET active=false, strava_data=null, update_indoor_ride=false, update_indoor_ride_data=null, chat_id=null, enable_activity_summary=false, updated=now() WHERE athlete_id={athlete_id}"
 
     API_TOKEN_EXCHANGE = 'https://www.strava.com/oauth/token'
