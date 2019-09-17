26fce572b815da20e626c3e456fc189380791a0b
pancham <panchamrb@phonepe.com>
2019-09-17 18:41:21 +0530

    Separate out challenges for ToK to a new class

--- app/commands/tok_odd_month.py
+++ app/commands/tok_odd_month.py
@@ -247,13 +247,13 @@ class ToKOddMonth:
                         list_swims_distance.append(activity["distance"])
                         list_swims_elevation.append(activity["elevation"])
 
-                activities_calendar[activity_day]["data"].update(
-                    {"total_distance": {"Ride": 0.0, "Run": 0.0, "Swim": 0.0}})
+                activities_calendar[activity_day]["data"].update({"total_distance": {"Ride": 0.0, "Run": 0.0,
+                                                                                     "Swim": 0.0}})
                 activities_calendar[activity_day]["data"]["total_distance"]["Ride"] = sum(list_rides_distance)
                 activities_calendar[activity_day]["data"]["total_distance"]["Run"] = sum(list_runs_distance)
                 activities_calendar[activity_day]["data"]["total_distance"]["Swim"] = sum(list_swims_distance)
-                activities_calendar[activity_day]["data"].update(
-                    {"total_elevation": {"Ride": 0.0, "Run": 0.0, "Swim": 0.0}})
+                activities_calendar[activity_day]["data"].update({"total_elevation": {"Ride": 0.0, "Run": 0.0,
+                                                                                      "Swim": 0.0}})
                 activities_calendar[activity_day]["data"]["total_elevation"]["Ride"] = sum(list_rides_elevation)
                 activities_calendar[activity_day]["data"]["total_elevation"]["Run"] = sum(list_runs_elevation)
                 activities_calendar[activity_day]["data"]["total_elevation"]["Swim"] = sum(list_swims_elevation)
