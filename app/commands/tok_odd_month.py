d94d589c1fd893a0e8b0b9c3d62612e6ac3113ff
pancham <panchamrb@phonepe.com>
2019-09-18 08:18:56 +0530

    Challenges summary

--- app/commands/tok_odd_month.py
+++ app/commands/tok_odd_month.py
@@ -426,8 +426,8 @@ class ToKOddMonth:
         if points["base"]["Ride"]["distance"] > 0:
             challenge_summary += "*Ride*:\n\n"
             challenge_summary += "_Base Points_:\n"
-            challenge_summary += "10 km = 1 Point: {}".format(points["base"]["Ride"]["distance"])
-            challenge_summary += "100 meters = 1 Point: {}".format(points["base"]["Ride"]["elevation"])
+            challenge_summary += "10 km = 1 Point: {}\n".format(points["base"]["Ride"]["distance"])
+            challenge_summary += "100 meters = 1 Point: {}\n".format(points["base"]["Ride"]["elevation"])
             challenge_summary += "1 activity = 1 Point: {}\n\n".format(points["base"]["Ride"]["activities"])
             challenge_summary += "_Bonus Points_:\n"
             challenge_summary += "Distance: {}\n".format(points["bonus"]["Ride"]["distance"])
